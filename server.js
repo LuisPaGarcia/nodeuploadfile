1bba7f8f37f7d44b488d70a794a8162ebc81b15d
LuisPa <diganluispa@gmail.com>
2020-06-06 17:37:21 -0600

    added file rad

--- server.js
+++ server.js
@@ -1,33 +1,31 @@
-var express     = require('express');
-var formidable  = require('formidable');
-const path      = require('path');
-const fs        = require('fs');
-var app         = express();
+var express = require('express');
+var formidable = require('formidable');
+const path = require('path');
+const fs = require('fs');
+var app = express();
 
-app.get('/', function (req, res){
-    res.sendFile(__dirname + '/index.html');
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/index.html');
 });
 
-app.post('/', function (req, res){
+app.post('/', function(req, res) {
+	if (!fs.existsSync(`./uploads`)) fs.mkdirSync(`./uploads`);
 
-    if (!fs.existsSync(`./uploads`))
-        fs.mkdirSync(`./uploads`);
+	var form = new formidable.IncomingForm();
 
-    var form = new formidable.IncomingForm();
+	form.parse(req);
 
-    form.parse(req);
+	form.on('file', function(name, file) {
+		console.log('Uploaded ' + file.name);
+		console.log({ file });
 
-    form.on('fileBegin', function (name, file){
-        file.path = __dirname + '/uploads/' + file.name;
-    });
+		const ret = fs.readFileSync(file.path);
+		console.log(ret.toString());
+	});
 
-    form.on('file', function (name, file){
-        console.log('Uploaded ' + file.name);
-    });
-
-    res.sendFile(__dirname + '/index.html');
+	res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(3000,()=>{
-    console.log('Listening on 3000 port');
+app.listen(3000, () => {
+	console.log('Listening on 3000 port');
 });
